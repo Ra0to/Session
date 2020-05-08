b5641675d92b6c1a5b74ae60ddb3925e2e3b4d39
mbatishchev <mbatishchev.craftgs@gmail.com>
2020-05-08 13:44:09 +0300

    Тесты

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -12,5 +12,35 @@ namespace TestProject1
 			var res = FileChecker.CheckCourse("31");
 			Assert.AreEqual(res, true);
 		}
+		[TestMethod]
+		public void TestMethod2()
+		{
+			var res = FileChecker.CheckMark("6");
+			Assert.AreEqual(res, false);
+		}
+		[TestMethod]
+		public void TestMethod3()
+		{
+			var res = FileChecker.CheckLesson("Hello");
+			Assert.AreEqual(res, true);
+		}
+		[TestMethod]
+		public void TestMethod4()
+		{
+			var res = FileChecker.CheckLastName("");
+			Assert.AreEqual(res, false);
+		}
+		[TestMethod]
+		public void TestMethod5()
+		{
+			var res = FileChecker.CheckDocument("_34./cx");
+			Assert.AreEqual(res, false);
+		}
+		[TestMethod]
+		public void TestMethod6()
+		{
+			var res = FileChecker.CheckGroup("343.34");
+			Assert.AreEqual(res, false);
+		}
 	}
 }
